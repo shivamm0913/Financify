1113b611f79f5073a166e372b9cd3af83b48e8ff
Shivam Kewat <shivamkewat0209@gmail.com>
2025-11-02 16:39:12 +0530

    serverjs changes 2

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 
-// app.listen(PORT, () => {
-//   console.log(`Server running on  http://localhost:${PORT}`);
-// });
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server running on  http://localhost:${PORT}`);
+});
+// module.exports = app;
