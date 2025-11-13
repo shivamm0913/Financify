1e45e478cdcabffa467680cc3a81c612ccced1f1
Shivam Kewat <shivamkewat0209@gmail.com>
2025-11-13 21:16:45 +0530

    styling update

--- frontend/src/components/Cards/TransactionInfoCard.jsx
+++ frontend/src/components/Cards/TransactionInfoCard.jsx
@@ -42,7 +42,7 @@ const TransactionInfoCard = ({
         <div className="flex items-center  gap-2">
           {!hideDeleteBtn && (
             <button
-              className="text-gray-400 hover:text-red-500 opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer"
+              className="md:text-gray-400 md:hover:text-red-500 text-red-500 md:opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer"
               onClick={onDelete}
             >
               <LuTrash2 size={18} />
