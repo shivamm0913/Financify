834c9c8a8ee751be1f3786e6aab5f17e23021fac
Shivam Kewat <shivamkewat0209@gmail.com>
2026-01-01 20:49:24 +0530

    vercel setup

--- backend/server.js
+++ backend/server.js
@@ -51,9 +51,10 @@ app.use("/api/v1/profile", userRoutes);
 //Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on  http://localhost:${PORT}`);
-});
-// module.exports = app;
+// app.listen(PORT, () => {
+//   console.log(`Server running on  http://localhost:${PORT}`);
+// });
+
+ module.exports = app;
