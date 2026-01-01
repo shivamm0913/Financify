976e84848f13269d3300e1dceffacb66aa318223
Shivam Kewat <shivamkewat0209@gmail.com>
2026-01-01 21:07:55 +0530

    vercel setup 2

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,28 @@
 const mongoose = require("mongoose");
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {});
-    console.log("MongoDB Connected");
-  } catch (error) {
-    console.error("Error connecting to the database", error);
-    process.exit(1);
+  if (cached.conn) {
+    // Already connected
+    return cached.conn;
   }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(process.env.MONGO_URI, {
+        bufferCommands: false,
+      })
+      .then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  console.log("MongoDB Connected");
+  return cached.conn;
 };
 
 module.exports = connectDB;
