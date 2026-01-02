06e6f5a04b47e841f04c81a8c7ccd9c763c74807
Shivam Kewat <shivamkewat0209@gmail.com>
2026-01-02 18:07:38 +0530

    emoji panel fix

--- frontend/src/components/EmojiPickerPopup.jsx
+++ frontend/src/components/EmojiPickerPopup.jsx
@@ -6,23 +6,38 @@ const EmojiPickerPopup = ({ icon, onSelect }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="flex flex-col md:flex-row items-start  gap-5 mb-6 ">
-      <div className="flex items-center gap-4 cursor-pointer" onClick={() => setIsOpen(true)}>
+    <div className="flex flex-col md:flex-row items-start  gap-5 mb-6 w-full">
+      <div
+        className="flex items-center gap-4 cursor-pointer"
+        onClick={() => setIsOpen(true)}
+      >
         <div className="w-10 h-10 flex  items-center justify-center text-2xl bg-green-50 text-primary roudned-lg">
-          {icon ? <img src={icon} alt="Icon" className="w-12 h-12" /> : <LuImage />}
+          {icon ? (
+            <img src={icon} alt="Icon" className="w-12 h-12" />
+          ) : (
+            <LuImage />
+          )}
         </div>
         <p className="">{icon ? "Change Icon" : "Pick Icon "}</p>
       </div>
 
       {isOpen && (
-        <div className="relative">
-          <button className="w-7 h-7 flex items-center justify-center bg-white border-gray-200 rounded-full absolute -top-2 -right-2 z-10 cursor-pointer " onClick={() => setIsOpen(false)}>
+        <div className="relative w-full max-w-[350px] md:max-w-none">
+          <button
+            className="w-7 h-7 flex items-center justify-center bg-white border-gray-200 rounded-full absolute -top-2 -right-2 z-10 cursor-pointer shadow-md"
+            onClick={() => setIsOpen(false)}
+          >
             <LuX />
           </button>
-          <EmojiPicker
-            open={isOpen}
-            onEmojiClick={(emoji) => onSelect(emoji?.imageUrl || "")}
-          />
+          <div className="w-full overflow-hidden">
+            <EmojiPicker
+              open={isOpen}
+              onEmojiClick={(emoji) => onSelect(emoji?.imageUrl || "")}
+              width="100%"
+              height={400}
+              style={{ maxWidth: "100%" }}
+            />
+          </div>
         </div>
       )}
     </div>
